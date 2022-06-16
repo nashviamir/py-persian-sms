0fa64203d323ec677f350d0f03f32640997e3edc
amir <nashviamir@gmail.com>
2022-06-16 11:36:15 +0430

    version number change

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 DESCRIPTION = 'A Python library for sending SMS via sms.ir API'
 LONG_DESCRIPTION = 'A package that allows you to send SMS via sms.ir API without direct exposure to the API.' 
 
